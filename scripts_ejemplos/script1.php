d4416a898f07af017f215a893eca4f42355af6c5
jeison14-16 <jeison14-16@hotmail.com>
2019-03-15 12:01:42 -0500

    introduccion PHP

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,15 @@
+<center>Programacion avanzada</center>
+<?php
+echo  "<center>Bienvenidos</center>";
+$nombre="peter";
+$apellido="Watson";
+$edad=30;
+$salario=800;
+
+echo "Nombre: ".$nombre."<br>";
+echo "Apellido: ".$apellido;
+echo "<br>Edad: ".$edad;
+echo "<br>Salario: ".$salario;
+
+?>
+<center>Fin de programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,24 @@
+
+<center><h1>CALCULADORA BASICA</center><br>
+
+  <?php
+  //esta mi primer calculadora
+$num1=10;
+$num2=5;
+
+//$suma=$num1+$num2;
+$resta=$num1-$num2;
+$multiplicacion=$num1*$num2;
+$division=$num1/$num2;
+
+
+echo "<center>";
+echo "La suma es: ".($num1+$num2);
+echo "<br>La resta es: ".$resta;
+echo "<br>La multiplicacion es: ".$multiplicacion;
+echo "<br>La division es: ".$division;
+
+echo "</center>";
+
+
+ ?>
